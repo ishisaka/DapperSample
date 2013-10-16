1d75e471909c2b4885e9c3031f0e2adfff66c777
ishisaka <ishisaka@gmail.com>
2013-10-16 18:15:29 +0900

    データ表示のラムダ椎の見直し

--- GetData/Program.cs
+++ GetData/Program.cs
@@ -20,12 +20,12 @@ namespace GetData
                     new {tm = DateTime.Now, desc = "Description"});
                 //データ抽出
                 var logs = con.Query<Log>("SELECT Id, TimeStamp, Description FROM Log");
-                logs.ToList().ForEach((l) => Console.WriteLine("{0}, {1}, {2}", l.Id, l.TimeStamp, l.Description));
+                logs.ToList().ForEach(l => Console.WriteLine("{0}, {1}, {2}", l.Id, l.TimeStamp, l.Description));
                 //ストアードプロシージャの実行
                 Console.WriteLine("ストアードプロシージャの実行");
                 var logs2 = con.Query<Log>("GetLogs", new { param1 = DateTime.Now.AddHours(1.0) },
                     commandType: CommandType.StoredProcedure);
-                logs2.ToList().ForEach((l) => Console.WriteLine("{0}, {1}, {2}", l.Id, l.TimeStamp, l.Description));
+                logs2.ToList().ForEach(l => Console.WriteLine("{0}, {1}, {2}", l.Id, l.TimeStamp, l.Description));
 
                 Console.WriteLine("終了するにはEnterキーを押して下さい。");
                 Console.ReadLine();
